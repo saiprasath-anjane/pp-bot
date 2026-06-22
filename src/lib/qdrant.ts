b7ca41ca183bb945e96133db1b98d400ddb3e356
Saiprasath <saiprasath.balasubramanian@anjanetech.com>
2026-06-22 15:52:59 +0530

    qdrant api key update

--- src/lib/qdrant.ts
+++ src/lib/qdrant.ts
@@ -1,6 +1,7 @@
 import { QdrantClient } from '@qdrant/js-client-rest';
-import { QDRANT_URL } from '$env/static/private';
+import { QDRANT_URL, QDRANT_API_KEY } from '$env/static/private';
 
 export const qdrant = new QdrantClient({
-  url: QDRANT_URL ?? 'http://localhost:6333'
+  url: QDRANT_URL ?? 'http://localhost:6333',
+  apiKey: QDRANT_API_KEY
 });
